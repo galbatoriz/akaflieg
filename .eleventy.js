be91d84405574117466983afd4c1d182a5bd7c19
Jens Birk Kurre <jens.birk.kurre@uni-oldenburg.de>
2025-08-25 14:55:57 +0200

    Fixes 3

--- .eleventy.js
+++ .eleventy.js
@@ -6,11 +6,10 @@ module.exports = function (eleventyConfig) {
 
     // Automatisches Prefix für Project Pages: /<repo>/, für User Pages: /
     const [owner, repo] = (process.env.GITHUB_REPOSITORY || "").split("/");
-    const isUserSite = repo && repo.toLowerCase() === `${(owner || "").toLowerCase()}.github.io`;
+    const isUserSite = repo && repo.toLowerCase() === `${(owner||"").toLowerCase()}.github.io`;
     const pathPrefix = isUserSite ? "/" : repo ? `/${repo}/` : "/";
-
     return {
-        dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
+        dir: { input: "src", includes: "_includes", output: "_site" },
         markdownTemplateEngine: "liquid",
         htmlTemplateEngine: "liquid",
         pathPrefix
